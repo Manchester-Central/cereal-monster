688cf3d8d0a8be354a8134598036fb26fd7f541a
C.H.A.O.S <Manchester-Central@users.noreply.github.com>
2025-08-14 19:42:37 -0400

    Servo fix

--- cerealmonster/cerealmonster.ino
+++ cerealmonster/cerealmonster.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
-#include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#include <avr/power.h>  // Required for 16 MHz Adafruit Trinket
 #endif
 #include <Adafruit_VS1053.h>
 #include <SD.h>
@@ -8,23 +8,23 @@
 #include <SPI.h>
 
 // AdaFruit MusicMaker mp3 player sheild
-#define SHIELD_RESET -1      // VS1053 reset pin (unused!)
-#define SHIELD_CS     7      // VS1053 chip select pin (output)
-#define SHIELD_DCS    6
-#define CARDCS        4      // Card chip select pin
-#define DREQ          3      // VS1053 Data request, ideally an Interrupt pin // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
+#define SHIELD_RESET -1  // VS1053 reset pin (unused!)
+#define SHIELD_CS 7      // VS1053 chip select pin (output)
+#define SHIELD_DCS 6
+#define CARDCS 4  // Card chip select pin
+#define DREQ 3    // VS1053 Data request, ideally an Interrupt pin // DREQ should be an Int pin, see http://arduino.cc/en/Reference/attachInterrupt
 // 2 x 24 LED AdaFruit NeoPixel rings (P1586)
-#define PIXEL_PIN     9      // Which pin on the Arduino is connected to the NeoPixels?
-#define NUMPIXELS    48      // Total number of pixels
+#define PIXEL_PIN 9   // Which pin on the Arduino is connected to the NeoPixels?
+#define NUMPIXELS 48  // Total number of pixels
 //servos
-#define LEFT_EYE     10
-#define RIGHT_EYE     5
+#define LEFT_EYE 10
+#define RIGHT_EYE 5
 // photo-resistor
-#define BEAM_SENSOR  A0
+#define BEAM_SENSOR A0
 
 // #define BEAM_THRESHOLD 600
 int BEAM_THRESHOLD = 600;
-#define VOLUME         20
+#define VOLUME 20
 
 Servo leftEye;
 Servo rightEye;
@@ -53,18 +53,20 @@ void setup() {
   pinMode(BEAM_SENSOR, INPUT);
 
   int average_value = lightCalibration();
-  BEAM_THRESHOLD = average_value; 
+  BEAM_THRESHOLD = average_value;
   Serial.print(BEAM_THRESHOLD);
 
-  if (! musicPlayer.begin()) { // initialise the music player
+  if (!musicPlayer.begin()) {  // initialise the music player
     Serial.println(F("Couldn't find VS1053, do you have the right pins defined?"));
-    while (1);
+    while (1)
+      ;
   }
   Serial.println(F("VS1053 found"));
 
   if (!SD.begin(CARDCS)) {
     Serial.println(F("SD failed, or not present"));
-    while (1);  // don't do anything more
+    while (1)
+      ;  // don't do anything more
   }
 
   musicPlayer.setVolume(VOLUME, VOLUME);
@@ -73,14 +75,14 @@ void setup() {
 }
 
 void eyecolor(uint8_t r, uint8_t g, uint8_t b) {
-  for (int i = 0; i < NUMPIXELS; i++) { // For each pixel...
+  for (int i = 0; i < NUMPIXELS; i++) {  // For each pixel...
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     pixels.setPixelColor(i, pixels.Color(r, g, b));
   }
   pixels.show();
 }
 
-const char * stateName(State_T s) {
+const char* stateName(State_T s) {
   if (s == hangry) {
     return "hangry";
   } else if (s == chewing) {
@@ -103,8 +105,7 @@ void changeState(State_T newState) {
   state = newState;
   stateStartTime = millis();
 
-  switch (newState)
-  {
+  switch (newState) {
     case hangry:
       hangryState_entry();
       break;
@@ -120,8 +121,8 @@ void changeState(State_T newState) {
 }
 
 void hangryState_entry() {
-  leftEye.write(90);// - 20);
-  rightEye.write(90);//  + 20);
+  leftEye.write(90 - 20);   // - 20);
+  rightEye.write(90 + 20);  //  + 20);
   eyecolor(20, 0, 0);
 }
 
@@ -131,8 +132,7 @@ void hangryState() {
   }
 }
 
-void chewingState_entry()
-{
+void chewingState_entry() {
   leftEye.write(90);
   rightEye.write(90);
   eyecolor(15, 5, 0);
@@ -145,10 +145,9 @@ void chewingState() {
   }
 }
 
-void happyState_entry()
-{
-  leftEye.write(90);// + 30);
-  rightEye.write(90);// - 30);
+void happyState_entry() {
+  leftEye.write(90 + 30);   // + 30);
+  rightEye.write(90 - 30);  // - 30);
   eyecolor(0, 20, 0);
   musicPlayer.startPlayingFile("/happy.mp3");
 }
@@ -163,12 +162,11 @@ void loop() {
   // put your main code here, to run repeatedly:
   if (state == hangry) {
     hangryState();
-  }  else if (state == chewing) {
+  } else if (state == chewing) {
     chewingState();
-  } else {
+  } else if (state == happy) {
     happyState();
   }
-
 }
 
 int lightCalibration() {
@@ -182,7 +180,7 @@ int lightCalibration() {
   delay(500);
   int value_5 = analogRead(BEAM_SENSOR);
   delay(500);
-  int average_value = ((value_1 + value_2 + value_3 + value_4 + value_5)/5) * 0.9;
+  int average_value = ((value_1 + value_2 + value_3 + value_4 + value_5) / 5) * 0.9;
   return average_value;
 }
 
